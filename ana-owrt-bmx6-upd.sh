fb041f8a61780f38ea04a3b7075893c48ad45e5c
Axel Neumann <neumann@cgws.de>
2016-02-02 14:14:22 +0100

    ana-owrt-bmx6-upd.sh

--- ana-owrt-bmx6-upd.sh
+++ ana-owrt-bmx6-upd.sh
@@ -3,11 +3,11 @@
 command=$0
 period=$1
 
-echo "command=$0 period=$1"
+echo "command=$command period=$period -> $((( -1 * $period )))"
 
 if [ "$period" ]; then
         echo starting...
-    while true; do time bmx6 -c descUpdate; sleep $period; done &
+    while true; do time bmx6 -c descUpdate; sleep $((( -1 * $period))); done &
 else
         pcs="$(ps | grep -e $command | grep -v grep | awk '{print $1}')"
         echo "stopping $pcs"
